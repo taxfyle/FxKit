a8fdfd5fbf1f1e9214ef9ce2fd1e686ba251cf48
Fabrício Godoy <skarllot@gmail.com>
2024-06-04 17:55:23 -0300

    feat(Option): Support OkOrElse with materialized error

--- src/FxKit/Option/Option.NaturalTransformations.cs
+++ src/FxKit/Option/Option.NaturalTransformations.cs
@@ -9,13 +9,14 @@ namespace FxKit;
 public static partial class Option
 {
     /// <summary>
-    ///     Maps the <see cref="Option{T}" /> to a <see cref="Result{T,E}" />.
+    /// Returns a result containing the value from an <see cref="Option{T}"/> if it exists.
+    /// Otherwise, returns a result with the specified error value.
     /// </summary>
-    /// <param name="source"></param>
-    /// <param name="selector"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="R"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">The type of the value in the result.</typeparam>
+    /// <typeparam name="R">The type of the error in the result.</typeparam>
+    /// <param name="source">The input <see cref="Option{T}"/>.</param>
+    /// <param name="selector">A function that provides the error value to return if the option is empty.</param>
+    /// <returns>A <see cref="Result{TOk,TErr}"/> containing either the value or the specified error.</returns>
     [DebuggerHidden]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [GenerateTransformer]
@@ -24,9 +25,30 @@ public static partial class Option
         Func<R> selector)
         where T : notnull
         where R : notnull =>
-        source.Match(
-            Some: Result<T, R>.Ok,
-            None: () => Err<T, R>(selector()));
+        source.TryGet(out var value)
+            ? Result<T, R>.Ok(value)
+            : Result<T, R>.Err(selector());
+
+    /// <summary>
+    /// Returns a result containing the value from an <see cref="Option{T}"/> if it exists.
+    /// Otherwise, returns a result with the specified error value.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in the result.</typeparam>
+    /// <typeparam name="R">The type of the error in the result.</typeparam>
+    /// <param name="source">The input <see cref="Option{T}"/>.</param>
+    /// <param name="invalidValue">The error value to return if the option is empty.</param>
+    /// <returns>A <see cref="Result{TOk,TErr}"/> containing either the value or the specified error.</returns>
+    [DebuggerHidden]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [GenerateTransformer]
+    public static Result<T, R> OkOr<T, R>(
+        this Option<T> source,
+        R invalidValue)
+        where T : notnull
+        where R : notnull =>
+        source.TryGet(out var value)
+            ? Result<T, R>.Ok(value)
+            : Result<T, R>.Err(invalidValue);
 
     /// <summary>
     ///     Maps the <see cref="Option{T}" /> to a <see cref="Validation{T,E}" />.
